0b258862fbe0746fa9d5104418e338a79ecbaaa2
luciano-cenatiempo <luciano.cenatiempo.dev@gmail.com>
2023-03-07 18:46:51 +0000

    Primer commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["logaliGroup/invoices/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"logaliGroup/invoices/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
